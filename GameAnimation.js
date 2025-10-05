527e9f2dab894adfeda51dc4dbf7f1310360cdb2
Fatima <f.f.jaffar@gmail.com>
2025-10-05 21:53:49 +0300

    switch truns and check for winner function

--- GameAnimation.js
+++ GameAnimation.js
@@ -148,7 +148,95 @@ const updateMessage = () => {
     }
   }
 }
+const switchTurns = () => {
+  if (winner === true){
+    return
+  }
+  if (turn === "red") {
+    turn === "yellow"
+  } else {
+    turn === "red"
+  }
+}
+const placePiece = (idx) => {
+  board[idx] = turn
+}
 
+const checkForWinner = () => {
+  if (
+    (board[0] !== "" && board[0] === board[1] && board[0] === board[2] && board[0] === board[3])
+    (board[1] !== "" && board[1] === board[2] && board[1] === board[3] && board[1] === board[4])
+    (board[2] !== "" && board[2] === board[3] && board[2] === board[4] && board[2] === board[5])
+    (board[3] !== "" && board[3] === board[4] && board[3] === board[5] && board[3] === board[6])
+    (board[7] !== "" && board[7] === board[8] && board[7] === board[9] && board[7] === board[10])
+    (board[8] !== "" && board[8] === board[9] && board[8] === board[10] && board[8] === board[11])
+    (board[9] !== "" && board[9] === board[10] && board[9] === board[11] && board[9] === board[12])
+    (board[10] !== "" && board[10] === board[11] && board[10] === board[12] && board[10] === board[13])
+    (board[14] !== "" && board[14] === board[15] && board[14] === board[16] && board[14] === board[17])
+    (board[15] !== "" && board[15] === board[16] && board[15] === board[17] && board[15] === board[18])
+    (board[16] !== "" && board[16] === board[17] && board[16] === board[18] && board[16] === board[19])
+    (board[17] !== "" && board[17] === board[18] && board[17] === board[19] && board[17] === board[20])
+    (board[21] !== "" && board[21] === board[22] && board[21] === board[23] && board[21] === board[24])
+    (board[22] !== "" && board[22] === board[23] && board[22] === board[24] && board[22] === board[25])
+    (board[23] !== "" && board[23] === board[24] && board[23] === board[25] && board[23] === board[26])
+    (board[24] !== "" && board[24] === board[25] && board[24] === board[26] && board[24] === board[27])
+    (board[28] !== "" && board[28] === board[29] && board[28] === board[30] && board[28] === board[30])
+    (board[29] !== "" && board[29] === board[30] && board[29] === board[31] && board[29] === board[32])
+    (board[30] !== "" && board[30] === board[31] && board[30] === board[32] && board[30] === board[33])
+    (board[31] !== "" && board[31] === board[32] && board[31] === board[33] && board[31] === board[34])
+    (board[35] !== "" && board[35] === board[36] && board[35] === board[37] && board[35] === board[38])
+    (board[36] !== "" && board[36] === board[37] && board[36] === board[38] && board[36] === board[39])
+    (board[37] !== "" && board[37] === board[38] && board[37] === board[39] && board[37] === board[40])
+    (board[38] !== "" && board[38] === board[39] && board[38] === board[40] && board[38] === board[41]) ///horizontal
+    (board[35] !== "" && board[35] === board[28] && board[35] === board[21] && board[35] === board[14])
+    (board[28] !== "" && board[28] === board[21] && board[28] === board[14] && board[28] === board[7])
+    (board[21] !== "" && board[21] === board[14] && board[21] === board[7] && board[21] === board[0])
+    (board[36] !== "" && board[36] === board[29] && board[36] === board[22] && board[36] === board[15])
+    (board[29] !== "" && board[29] === board[22] && board[29] === board[15] && board[29] === board[8])
+    (board[22] !== "" && board[22] === board[15] && board[22] === board[8] && board[22] === board[1])
+    (board[37] !== "" && board[37] === board[30] && board[37] === board[23] && board[37] === board[16])
+    (board[30] !== "" && board[30] === board[23] && board[30] === board[16] && board[30] === board[9])
+    (board[23] !== "" && board[23] === board[16] && board[23] === board[9] && board[23] === board[2])
+    (board[38] !== "" && board[38] === board[31] && board[38] === board[24] && board[38] === board[17])
+    (board[31] !== "" && board[31] === board[24] && board[31] === board[17] && board[31] === board[10])
+    (board[24] !== "" && board[24] === board[17] && board[24] === board[10] && board[24] === board[3])
+    (board[39] !== "" && board[39] === board[32] && board[39] === board[25] && board[39] === board[18])
+    (board[32] !== "" && board[32] === board[25] && board[32] === board[18] && board[32] === board[11])
+    (board[25] !== "" && board[25] === board[18] && board[25] === board[11] && board[25] === board[4])
+    (board[40] !== "" && board[40] === board[33] && board[40] === board[26] && board[40] === board[19])
+    (board[33] !== "" && board[33] === board[26] && board[33] === board[19] && board[33] === board[12])
+    (board[26] !== "" && board[26] === board[19] && board[26] === board[12] && board[26] === board[5])
+    (board[41] !== "" && board[41] === board[34] && board[41] === board[27] && board[41] === board[20])
+    (board[34] !== "" && board[34] === board[27] && board[34] === board[20] && board[34] === board[13])
+    (board[27] !== "" && board[27] === board[20] && board[27] === board[13] && board[27] === board[6]) ///vertical
+    (board[35] !== "" && board[35] === board[29] && board[35] === board[23] && board[35] === board[17])
+    (board[23] !== "" && board[23] === board[17] && board[23] === board[11] && board[23] === board[5])
+    (board[6] !== "" && board[6] === board[12] && board[6] === board[18] && board[6] === board[24])
+    (board[12] !== "" && board[12] === board[18] && board[12] === board[24] && board[12] === board[30])
+    (board[18] !== "" && board[18] === board[24] && board[18] === board[30] && board[18] === board[36])
+    (board[28] !== "" && board[28] === board[22] && board[28] === board[16] && board[28] === board[10])
+    (board[22] !== "" && board[22] === board[16] && board[22] === board[10] && board[22] === board[4])
+    (board[21] !== "" && board[21] === board[15] && board[21] === board[9] && board[21] === board[3])
+    (board[12] !== "" && board[12] === board[18] && board[12] === board[24] && board[12] === board[30])
+    (board[37] !== "" && board[37] === board[31] && board[37] === board[25] && board[37] === board[19])
+    (board[31] !== "" && board[31] === board[25] && board[31] === board[19] && board[31] === board[13])
+    (board[38] !== "" && board[38] === board[32] && board[38] === board[26] && board[38] === board[20])
+    (board[41] !== "" && board[41] === board[33] && board[41] === board[25] && board[41] === board[17])
+    (board[33] !== "" && board[33] === board[25] && board[33] === board[17] && board[33] === board[9])
+    (board[25] !== "" && board[25] === board[17] && board[25] === board[9] && board[25] === board[1])
+    (board[27] !== "" && board[27] === board[19] && board[27] === board[11] && board[27] === board[3])
+    (board[34] !== "" && board[34] === board[26] && board[34] === board[18] && board[34] === board[10])
+    (board[26] !== "" && board[26] === board[18] && board[26] === board[10] && board[26] === board[2])
+    (board[40] !== "" && board[40] === board[32] && board[40] === board[24] && board[40] === board[16])
+    (board[32] !== "" && board[32] === board[24] && board[32] === board[16] && board[32] === board[8])
+    (board[24] !== "" && board[24] === board[16] && board[24] === board[8] && board[24] === board[0])
+    (board[39] !== "" && board[39] === board[31] && board[39] === board[23] && board[39] === board[15])
+    (board[31] !== "" && board[31] === board[23] && board[31] === board[15] && board[31] === board[7])
+    (board[38] !== "" && board[28] === board[30] && board[38] === board[22] && board[38] === board[14]) ///diagonally
+  ){
+    winner === true
+  }
+}
 
 
 
